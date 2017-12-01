214d2a7cf9456b7f9fcccf113a663995874aa783
Romario Estrella <rde16@my.fsu.edu>
2017-12-01 10:14:16 -0500

    makefile complete

---
+++ userland/testbin/meld/meld.c
@@ -0,0 +1,20 @@
+//meld.c
+//created by Romario Estrella-Ramos
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <err.h>
+
+int
+main(int argc, char *argv[])
+{
+
+  const char *
+
+
+  
+  printf("Passed melding test.\n");
+  return 0;
+}
+
